0ecf44dba5ca1fdefc28bd8d3311a318b5901563
Alex Zappa <alex@zappa.dev>
2023-10-23 17:20:41 +0200

    update TW config and update navbar and meta

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
 	title: "Serverless Gems for Netlify",
-  suffix: " | Serverless Gems",
+  suffix: " | Serverless Gems for Netlify",
 	url: "https://serverless-gems.netlify.app/",
 	language: "en",
 	description: "Serverless Gems is a curated collection of open-source projects for building serverless web applications! 🌐💡 JavaScript-powered and designed for easy deployment, maintenance, and scalability.",
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,21 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    aspectRatio: {
+      auto: "auto",
+      box: "1",
+      landscape: "4/3",
+      portrait: "3/4",
+      video: "16/9",
+    },
     extend: {
-      aspectRatio: {
-        auto: "auto",
-        box: "1",
-        landscape: "4/3",
-        portrait: "3/4",
-        video: "16/9",
-      },
     },
   },
   plugins: [
