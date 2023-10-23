40ad6b697d2408b1960084cc3072d2a334317ddc
Alex Zappa <alex@zappa.dev>
2023-10-23 03:37:04 +0200

    cleanup tracker

--- src/assets/js/modules/tracker.js
+++ src/assets/js/modules/tracker.js
@@ -29,56 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   enableAutoOutboundTracking();
 
-  // track File Download
-  document.body.addEventListener("click", (event) => {
-    if (
-      event.target.tagName === "A" &&
-      event.target.href.startsWith("http")
-    ) {
-      const fileExtensions = [
-        "pdf",
-        "xlsx",
-        "docx",
-        "txt",
-        "rtf",
-        "csv",
-        "exe",
-        "key",
-        "pps",
-        "ppt",
-        "pptx",
-        "7z",
-        "pkg",
-        "rar",
-        "gz",
-        "zip",
-        "avi",
-        "mov",
-        "mp4",
-        "mpeg",
-        "wmv",
-        "midi",
-        "mp3",
-        "wav",
-        "wma",
-      ];
-      const fileExtension = event.target.href.split(".").pop();
-      if (fileExtensions.includes(fileExtension)) {
-        trackEvent("File Download", {
-          props: {
-            url: event.target.href,
-          },
-        });
-
-        setTimeout(() => {
-          location.href = this.href;
-        }, 150);
-
-        event.preventDefault();
-      }
-    }
-  });
-
   // track 404 page
   if (document.body.classList.contains("page-404")) {
     trackEvent("404", {
